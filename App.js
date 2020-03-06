0887cb547ae1188d80fa23765095c2d31afeeb43
Rashmiranjan00 <rr.pradhan00@gmail.com>
2020-03-06 14:15:37 +0530

    Code organized

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import {NetInfo} from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
-import {createStackNavigator} from 'react-navigation-stack';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-// import Icon from "react-native-vector-icons/MaterialIcons";
-import HomeScreen from './components/HomeScreen';
-import SettingScreen from './components/SettingScreen';
-import DetailsScreen from './components/DetailsScreen';
+import AppContainer from './utils/AppContainer';
 
 const CheckConnectivity = () => {
   NetInfo.isConnected.fetch().then(isConnected => {
@@ -18,43 +11,6 @@ const CheckConnectivity = () => {
     }
   });
 };
-
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
-  Details: DetailsScreen,
-});
-
-const getTabBarIcon = (navigation, focused, tintColor) => {
-  const routeName = navigation.state;
-  let IconComponent = Ionicons;
-  let iconName;
-  if (routeName == 'Home') {
-    iconName = `home${focused ? '' : '-outline'}`;
-  } else if (routeName === 'Settings') {
-    iconName = `cog${focused ? '' : '-outline'}`;
-  }
-  return <IconComponent name={iconName} size={25} color={tintColor} />;
-};
-
-const TabNav = createMaterialBottomTabNavigator(
-  {
-    Home: HomeStack,
-    Settings: SettingScreen,
-  },
-  {
-    defaultNavigationOptions: ({navigation}) => ({
-      tabBarIcon: ({focused, tintColor}) => {
-        getTabBarIcon(navigation, focused, tintColor);
-      },
-    }),
-    tabBarOptions: {
-      activeTintColor: 'tomato',
-      inactiveTintColor: 'gray',
-    },
-  },
-);
-
-const AppContainer = createAppContainer(TabNav);
 
 class App extends React.Component {
   constructor(props) {
---
+++ utils/AppContainer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {createAppContainer} from 'react-navigation';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
+import {createStackNavigator} from 'react-navigation-stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+// import Icon from "react-native-vector-icons/MaterialIcons";
+import HomeScreen from '.././screens/HomeScreen';
+import SettingScreen from '.././screens/SettingScreen'
+import DetailsScreen from '.././screens/DetailsScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  Details: DetailsScreen,
+});
+
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const routeName = navigation.state;
+  let IconComponent = Ionicons;
+  let iconName;
+  if (routeName == 'Home') {
+    iconName = `home${focused ? '' : '-outline'}`;
+  } else if (routeName === 'Settings') {
+    iconName = `cog${focused ? '' : '-outline'}`;
+  }
+  return <IconComponent name={iconName} size={25} color={tintColor} />;
+};
+
+const TabNav = createMaterialBottomTabNavigator(
+  {
+    Home: HomeStack,
+    Settings: SettingScreen,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarIcon: ({focused, tintColor}) => {
+        getTabBarIcon(navigation, focused, tintColor);
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  },
+);
+
+const AppContainer = createAppContainer(TabNav);
+
+export default AppContainer;
